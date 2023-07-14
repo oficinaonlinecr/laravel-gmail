17c2f67ce6ae2dfc2c12a3ace1fc6a8a724a6b42
oficinaonlinecr <110751854+oficinaonlinecr@users.noreply.github.com>
2023-07-14 15:38:40 -0600

    Update GmailConnection.php
    Read storage from config file

--- src/GmailConnection.php
+++ src/GmailConnection.php
@@ -149,7 +149,7 @@ class GmailConnection extends Google_Client
 	 */
 	public function saveAccessToken(array $config)
 	{
-		$disk = Storage::disk('local');
+		$disk = Storage::disk($this->_config['gmail.storage']);
 		$fileName = $this->getFileName();
 		$file = "gmail/tokens/$fileName.json";
 		$allowJsonEncrypt = $this->_config['gmail.allow_json_encrypt'];
